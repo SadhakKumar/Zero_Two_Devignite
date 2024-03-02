9847462606b76bae6cf1340a691ad669071e6298
taufique2208 <ansaritaufique379@gmail.com>
2024-03-02 19:26:00 +0530

    added cors

--- flask/app.py
+++ flask/app.py
@@ -1,6 +1,10 @@
 from flask import Flask,request,jsonify;
 # import util;
-app=Flask(__name__)
+# app=Flask(__name__)
+from flask_cors import CORS 
+
+app = Flask(__name__)
+CORS(app) 
 
 import json;
 import pickle;
