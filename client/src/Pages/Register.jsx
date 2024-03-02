20231e8b1775c8deb46e5a12666799ebe8706dc4
taufique2208 <ansaritaufique379@gmail.com>
2024-03-02 15:32:33 +0530

    added login n register

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -245,7 +245,7 @@ import { auth, firestore, setDoc, doc } from "../firebase"; // Assuming you have
                 } className="text-sm focus:shadow-soft-primary-outline leading-5.6 ease-soft block w-full appearance-none rounded-lg border border-solid border-gray-300 bg-white bg-clip-padding py-2 px-3 font-normal text-gray-700 transition-all focus:border-fuchsia-300 focus:bg-white focus:text-gray-700 focus:outline-none focus:transition-shadow" type="password"/>
             </div>
           <label>
-            Role:
+            Vehicle Model:
             <select
               value={values.model}
               onChange={(event) =>
